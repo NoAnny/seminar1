0d7f6642e83ee8dfa06d3e4ca52ae64189b41516
NoAnnY <noanny@mail.ru>
2022-11-08 00:56:58 +0300

    rename

---
+++ Task2/task2.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел
+Console.WriteLine("Enterin 1st number");
+double number1 = double.Parse(Console.ReadLine ()!);
+Console.WriteLine("Enterin 2nd number");
+double number2 = double.Parse(Console.ReadLine ()!);
+Console.WriteLine("Enterin 3d number");
+double number3 = double.Parse(Console.ReadLine ()!);
+double max = number1;
+if(number2 > max)
+{
+    max = number2;
+}
+if(number3 > max)
+{
+    max = number3;
+}
+Console.WriteLine($"The biggest is {max}");
